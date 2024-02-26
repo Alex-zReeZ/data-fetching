8a7c2971716379396609f9ae97b073012664dd14
Alex-zReeZ <alexbeck220@gmail.com>
2024-02-26 09:13:00 +0100

    fix : when clicking on navbar button, all the element are centeref in the middle one under the other

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,41 +1,44 @@
 import React, { useState } from "react";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <nav className="flex items-center justify-between p-6 bg-gray-800 text-white w-full">
-      <h1 className="text-2xl font-bold">Logo</h1>
+    const toggleNavbar = () => {
+        console.log("clicked");
+        setIsOpen(!isOpen);
+    }
 
-      <div
-          id="menu"
-        onClick={() => setIsOpen(true)}
-        className="lg:hidden cursor-pointer"
-      >
-        <div className="w-6 h-0.5 bg-white mb-1"></div>
-        <div className="w-6 h-0.5 bg-white mb-1"></div>
-        <div className="w-6 h-0.5 bg-white"></div>
-      </div>
+    return (
+        <nav className={`flex ${isOpen ? 'flex-col gap-5' : 'flex-row '} items-center justify-between p-6 bg-gray-800 text-white w-full`}>
+            <h1 className="text-2xl font-bold">Logo</h1>
 
-      <ul
-        className={`${
-          isOpen ? "block" : "hidden"
-        } lg:flex lg:items-center lg:space-x-8`}
-      >
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-      </ul>
-    </nav>
-  );
+            <div
+                id="menu"
+                onClick={() => toggleNavbar()}
+                className="lg:hidden cursor-pointer flex flex-col"
+            >
+                <div className="w-6 h-0.5 bg-white mb-1"></div>
+                <div className="w-6 h-0.5 bg-white mb-1"></div>
+                <div className="w-6 h-0.5 bg-white"></div>
+            </div>
+
+            <ul
+                className={`${
+                    isOpen ? "block" : "hidden"
+                } lg:flex lg:items-center lg:space-x-8`}
+            >
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Navbar;
-
-
