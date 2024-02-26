164f798e1185bde7b3c820c6086e1e747266332a
Alex-zReeZ <alexbeck220@gmail.com>
2024-02-26 09:19:35 +0100

    feat : when navbar menu in responsiv is open the button is changed to a cross

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,9 +17,17 @@ const Navbar = () => {
                 onClick={() => toggleNavbar()}
                 className="lg:hidden cursor-pointer flex flex-col"
             >
-                <div className="w-6 h-0.5 bg-white mb-1"></div>
-                <div className="w-6 h-0.5 bg-white mb-1"></div>
-                <div className="w-6 h-0.5 bg-white"></div>
+                {isOpen ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-white">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                ) : (
+                    <>
+                        <div className="w-6 h-0.5 bg-white mb-1"></div>
+                        <div className="w-6 h-0.5 bg-white mb-1"></div>
+                        <div className="w-6 h-0.5 bg-white"></div>
+                    </>
+                )}
             </div>
 
             <ul
