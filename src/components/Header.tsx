e1372c4c115c530c3bbaf8f1ad38bf8561e6d573
Alex-zReeZ <alexbeck220@gmail.com>
2024-03-04 13:37:55 +0100

    feat : you can change Home page lang in French or English

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,18 @@ import { useState } from "react";
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [lang, setLang] = useState('fr');
 
     const toggleNavbar = () => {
         setIsOpen(!isOpen);
     };
 
+    const getLangPrefix = () => (lang === 'fr' ? '' : '/en');
+
     return (
         <>
             <nav
-                className={`flex ${isOpen ? 'block pr-10' : 'flex-row '} p-6 bg-gray-800 text-white w-full transition-all duration-300`}>
+                className={`flex ${isOpen ? 'block pr-10 ' : 'flex-row px-80'} p-6 bg-gray-800 text-white w-full transition-all duration-300`}>
                 <img src="/favicon.svg" alt="logo" className="w-12 sm:ml-32 lg:ml-0" />
 
                 <div
@@ -44,21 +47,26 @@ const Navbar = () => {
                     className={`${isOpen ?
                         "transform translate-x-0 transition-all duration-500 ease-in-out flex gap-10 flex-col text-xl text-center mt-24 pt-20 z-30 lg:static lg:mt-0 lg:pt-0 lg:translate-x-0 lg:flex lg:items-center lg:space-x-8 absolute bg-gray-800 text-white w-96 h-full left-0 top-0 overflow-auto"
                         :
-                        "-translate-x-full flex gap-10 flex-col text-xl text-center mt-24 pt-20 z-30 lg:static lg:mt-0 lg:p-0 lg:translate-x-0 lg:flex lg:flex-row lg:items-center lg:pt-1.5 lg:justify-end absolute bg-gray-800 text-white w-96 h-full sm:left-0 sm:top-0 lg:right-0 overflow-auto "
+                        "-translate-x-full flex gap-10 flex-col text-xl text-center mt-24 pt-20 z-30 lg:static lg:mt-0 lg:pl-28 lg:translate-x-0 lg:flex lg:flex-row lg:w-full lg:items-center lg:pt-1.5  absolute bg-gray-800 text-white w-96 h-full sm:left-0 sm:top-0 lg:right-0 overflow-auto "
                         } 
                 lg:flex lg:items-center lg:space-x-8 `
                     }
                 >
                     <li>
-                        <a href="/" className="hover:text-gray-400">Home</a>
+                        <a href={`${getLangPrefix()}/`} className="hover:text-gray-400">Home</a>
                     </li>
                     <li>
-                        <a href="/login" className="hover:text-gray-400">Login Page</a>
+                        <a href={`${getLangPrefix()}/login`} className="hover:text-gray-400">Login Page</a>
                     </li>
                     <li>
-                        <a href="/404" className="hover:text-gray-400">Contact</a>
+                        <a href={`${getLangPrefix()}/404`} className="hover:text-gray-400">Contact</a>
                     </li>
                 </ul>
+                <button className="">
+                            <a href={`${getLangPrefix()}/`} onClick={() => setLang(lang === 'en' ? 'fr' : 'en')}>
+                                {getLangPrefix() === '/en' ? 'Français' : 'English'}
+                            </a>
+                        </button>
             </nav>
 
             <ul
@@ -78,6 +86,13 @@ const Navbar = () => {
                 </li>
                 <li>
                     <a href="#" className="hover:text-gray-400">Contact</a>
+                </li>
+                <li>
+                    <button className="">
+                        <a href="/">
+                            test
+                        </a>
+                    </button>
                 </li>
             </ul>
         </>
